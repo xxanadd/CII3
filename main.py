773f4da4f21349b88fac02313699b81771fafb25
Stepan <stepan.dubrov@gmail.com>
2023-09-24 23:38:52 +0300

    string parser

--- main.py
+++ main.py
@@ -2,10 +2,24 @@ from pyswip import Prolog
 
 prolog = Prolog()
 prolog.consult("lab1.pl")
-c = prolog.query("games_by_developer_on_platform(rockstar, pc, Games).")
 
-game_names = []
-for result in c:
-    game_names.append(result["Games"])
+# Запрос пользовательского ввода
+user_input = input("Введите строку в формате 'Игры разработанные компанией {название компании} для {название платформы}': \n")
 
-print(game_names)
+# Разбиваем введенную строку на части
+parts = user_input.split()
+if len(parts) == 6 and parts[0] == 'Игры' and parts[1] == 'разработанные' and parts[2] == 'компанией' and parts[4] == 'для':
+    developer = parts[3]
+    platform = parts[5]
+
+    # Строим запрос на основе извлеченных значений
+    query_str = f"games_by_developer_on_platform({developer}, {platform}, Games)."
+
+    # Запрашиваем данные у Prolog
+    res = next(prolog.query(query_str))["Games"]
+    if len(res) == 0:
+        print(f"Компанией {parts[3]} не было разработано ни одной игры на платформе {parts[5]}")
+    else:
+        print(" ".join(res))
+else:
+    print("Неверный формат ввода. Пожалуйста, используйте формат 'Игры разработанные компанией {название компании} для {название платформы}'")
